c4d429dad71fe4286393a0312d38c8af9b84101e
Kishore298 <kishore103345@gmail.com>
2024-10-04 19:14:44 +0530

    addes forms

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const options = {
-  serverSelectionTimeoutMS: 60000 
+  serverSelectionTimeoutMS: 30000
 };
 
 const connectDB = async () => {
@@ -9,10 +9,11 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (error) {
-    console.error('MongoDB connection failed');
+    console.error('MongoDB connection failed',error.message);
     process.exit(1);
   }
 };
+mongoose.set('debug', true);
 
 module.exports = connectDB;
 
